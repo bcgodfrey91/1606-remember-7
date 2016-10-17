d9e47aacd07b74d728708fe4ba5f610918281d78
Ben Godfrey <bcgodfrey91@gmail.com>
2016-10-17 14:42:37 -0600

    Remove inject, tagName, and classNames from new.js. Remove path from 'new' in the router.

--- app/controllers/reminders/new.js
+++ app/controllers/reminders/new.js
@@ -1,15 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
-  store: Ember.inject.service(),
-
-  tagName: 'section',
-  classNames: ['whatever'],
-
-  title: '',
-  date: '',
-  notes: '',
-    
+export default Ember.Controller.extend({    
   actions: {
     addReminder() {
       const reminder = this.getProperties('title', 'date', 'notes');
