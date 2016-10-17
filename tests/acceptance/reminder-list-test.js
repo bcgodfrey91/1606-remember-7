c96f762508e647d9b383c46bd4a2a8f990ef20e8
Mike Sausa <mikejsausa@gmail.com>
2016-10-17 15:47:15 -0600

    added test for edit

--- tests/acceptance/reminder-list-test.js
+++ tests/acceptance/reminder-list-test.js
@@ -41,20 +41,32 @@ test('creating an area to make a new reminder', function(assert) {
 
   andThen(function() {
     assert.equal(currentURL(), '/new');
-    assert.equal(Ember.$('.add-reminder-form').length,1)
+    assert.equal(Ember.$('.add-reminder-form').length,1);
   });
 });
 
 test('creating an new reminder', function(assert) {
   visit('/new');
 
-  fillIn('.spec-input-title.ember-view.ember-text-field', 'Call Mike')
-  fillIn('.spec-input-date', '11/11/2016')
-  fillIn('.spec-input-notes', 'Party')
-
+  fillIn('.spec-input-title.ember-view.ember-text-field', 'Call Mike');
+  fillIn('.spec-input-date', '11/11/2016');
+  fillIn('.spec-input-notes', 'Party');
 
   andThen(function() {
     assert.equal(currentURL(), '/new');
-    assert.equal(Ember.$('.spec-input-title').value, 'Call Mike');
+    assert.equal(Ember.$('.spec-input-title').val(), 'Call Mike');
+  });
+});
+
+test('editing a reminder', function(assert) {
+  visit('/');
+  click('.spec-reminder-item:first');
+  click('.edit-button');
+  fillIn('.edit-title', 'What up Mike?!');
+  click('.save-button');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/1');
+    assert.equal(Ember.$('.spec-reminder-item:last').text().trim(), 'What up Mike?!');
   });
 });
